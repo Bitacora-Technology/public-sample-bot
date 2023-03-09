7261a47ece0ea3e0d2b8fa49ed4c6399ba49d8a6
galind <guillem.yt1@gmail.com>
2023-03-09 01:33:44 +0100

    Added manage embed view structure

--- cogs/embeds.py
+++ cogs/embeds.py
@@ -4,14 +4,14 @@ from bot import Bot
 import discord
 
 
-def create_embed(bot: Bot, embed_info: dict) -> discord.Embed:
+def create_embed(embed_info: dict) -> discord.Embed:
     embed = discord.Embed(
         title=embed_info['title'],
         description=embed_info['description'],
-        color=bot.color
+        color=embed_info['color']
     )
 
-    embed.set_footer(text='https://bitacora.gg', icon_url=bot.user.avatar)
+    embed.set_footer(text='https://bitacora.gg', icon_url=embed_info['avatar'])
 
     embed.set_image(url=embed_info['image_url'])
     embed.set_thumbnail(url=embed_info['thumbnail_url'])
@@ -44,11 +44,63 @@ class CreateEmbedModal(discord.ui.Modal):
             'description': self.description.value,
             'field_list': [],
             'image_url': '',
-            'thumbnail_url': ''
+            'thumbnail_url': '',
+            'color': self.bot.color,
+            'avatar': self.bot.user.avatar
         }
 
-        embed = create_embed(self.bot, embed_info)
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+        embed = create_embed(embed_info)
+        view = ConfigureEmbedView(embed_info)
+        await interaction.response.send_message(
+            embed=embed, view=view, ephemeral=True
+        )
+
+
+class ManageFieldsButton(discord.ui.Button):
+    def __init__(self, embed_info: dict) -> None:
+        super().__init__(
+            label='Manage fields', style=discord.ButtonStyle.primary
+        )
+        self.embed_info = embed_info
+
+
+class ManageImageButton(discord.ui.Button):
+    def __init__(self, embed_info: dict) -> None:
+        image_url = embed_info['image_url']
+        if image_url == '':
+            label = 'Add image'
+            style = discord.ButtonStyle.success
+        else:
+            label = 'Remove image'
+            style = discord.ButtonStyle.danger
+        super().__init__(label=label, style=style)
+
+
+class ManageThumbnailButton(discord.ui.Button):
+    def __init__(self, embed_info: dict) -> None:
+        thumbnail_url = embed_info['image_url']
+        if thumbnail_url == '':
+            label = 'Add thumbnail'
+            style = discord.ButtonStyle.success
+        else:
+            label = 'Remove thumbnail'
+            style = discord.ButtonStyle.danger
+        super().__init__(label=label, style=style)
+
+
+class SendEmbedButton(discord.ui.Button):
+    def __init__(self, embed_info: dict) -> None:
+        super().__init__(label='Send embed', style=discord.ButtonStyle.primary)
+        self.embed_info = embed_info
+
+
+class ConfigureEmbedView(discord.ui.View):
+    def __init__(self, embed_info: dict) -> None:
+        super().__init__(timeout=None)
+        self.add_item(ManageFieldsButton(embed_info))
+        self.add_item(ManageImageButton(embed_info))
+        self.add_item(ManageThumbnailButton(embed_info))
+        self.add_item(SendEmbedButton(embed_info))
 
 
 @app_commands.guild_only()
@@ -58,8 +110,8 @@ class Embeds(commands.GroupCog, group_name='embeds'):
         self.bot = bot
 
     @app_commands.command()
-    async def send(self, interaction: discord.Interaction) -> None:
-        """Create a custom embed"""
+    async def create(self, interaction: discord.Interaction) -> None:
+        """Send a custom embed"""
         modal = CreateEmbedModal(self.bot)
         await interaction.response.send_modal(modal)
 
