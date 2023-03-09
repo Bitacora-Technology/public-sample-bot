91fbd0778b7e7331230e0334abed131e6f818c95
galind <guillem.yt1@gmail.com>
2023-03-09 20:40:14 +0100

    Added giveaway class

--- cogs/utils/mongo.py
+++ cogs/utils/mongo.py
@@ -5,6 +5,22 @@ client = motor.AsyncIOMotorClient('mongodb://localhost:27017')
 db = client['public-sample']
 
 
+class Giveaway:
+    def __init__(self, giveaway: int = 0) -> None:
+        self.giveaways = db['giveaways']
+        self.filter = {'_id': giveaway}
+
+    async def create(self, query: dict) -> None:
+        await self.giveaways.insert_one(query)
+
+    async def check(self) -> dict:
+        return await self.giveaways.find_one(self.filter)
+
+    async def update(self, query: dict, method: str = 'set') -> None:
+        update_query = {f'${method}': query}
+        await self.giveaways.update_one(self.filter, update_query, upsert=True)
+
+
 class User:
     def __init__(self, user: int = 0) -> None:
         self.users = db['users']
