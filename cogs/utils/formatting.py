a727c4c93367b6a5330222fa110c8617a9ac8e4a
galind <guillem.yt1@gmail.com>
2023-03-13 21:14:57 +0100

    Added poll emojis

--- cogs/utils/formatting.py
+++ cogs/utils/formatting.py
@@ -3,3 +3,9 @@ bot_avatar_url = (
     'https://cdn.discordapp.com/avatars/1053306790026149978/'
     'e2aa0070242d8cfae68e6af0300e26e1.png'
 )
+poll_emoji_dict = {
+    '100': '<:100percent:1084931195927793755>',
+    '75': '<:75percent:1084931197114794146>',
+    '50': '<:50percent:1084931199740428348>',
+    '25': '<:25percent:1084931201162285156>'
+}
