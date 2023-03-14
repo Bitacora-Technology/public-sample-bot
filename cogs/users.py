f3e10163476a6985c352883ca2ec77a43ab8606b
galind <guillem.yt1@gmail.com>
2023-03-14 03:30:19 +0100

    Added simple embed function and invite command

--- cogs/users.py
+++ cogs/users.py
@@ -9,10 +9,26 @@ import discord
 class Users(commands.Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
+        self.invite_url = (
+            'https://discord.com/api/oauth2/authorize?client_id=1085024816119156808'
+            '&permissions=8&scope=bot%20applications.commands'
+        )
 
     @app_commands.command()
     async def help(self, interaction: discord.Interaction) -> None:
         """Get the most out of the bot"""
+
+    def simple_embed(self, title: str, description: str) -> discord.Embed:
+        embed = discord.Embed(
+            title=title,
+            description=description,
+            color=formatting.embed_color_dec
+        )
+        embed.set_footer(
+            text='https://bitacora.gg', icon_url=formatting.bot_avatar_url
+        )
+
+        return embed
 
     @app_commands.command()
     async def balance(self, interaction: discord.Interaction) -> None:
@@ -29,7 +45,15 @@ class Users(commands.Cog):
         emoji = guild_info.get('emoji', '')
 
         content = f'Your balance is {balance} {emoji}'
-        await interaction.response.send_message(content, ephemeral=True)
+        embed = self.simple_embed('Coin balance', content)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+    @app_commands.command()
+    async def invite(self, interaction: discord.Interaction) -> None:
+        """Add the bot to your server"""
+        content = f'[[Click here]]({self.invite_url})'
+        embed = self.simple_embed('Add bot', content)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 async def setup(bot: Bot) -> None:
